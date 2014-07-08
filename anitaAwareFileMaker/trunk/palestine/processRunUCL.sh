b14a99a05a771d706af0ec9ce7c7a4c592c6d371
rjn <rjn@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2014-07-08 14:53:02 +0000

    Adding UCL script

---
+++ anitaAwareFileMaker/trunk/palestine/processRunUCL.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ "$1" = "" ]
+then
+   echo "usage: `basename $0` <run no>" 1>&2
+   exit 1
+fi
+
+
+
+RUN=$1
+BASE_DIR=/unix/anita2/palestine14
+RAW_RUN_DIR=${BASE_DIR}/raw/run${RUN}
+EVENT_BASE_DIR=${BASE_DIR}/root
+ROOT_RUN_DIR=${EVENT_BASE_DIR}/run${RUN}
+
+
+#Step 2: Generate the AWARE Files
+cd /home/rjn/anita/anitaAwareFileMaker/
+source setupAwareVariablesUCL.sh
+if [ -d "$ROOT_RUN_DIR" ]; then
+    ./makeHeaderJsonFiles ${ROOT_RUN_DIR}/headFile${RUN}.root 
+    ./makePrettyHkJsonFiles ${ROOT_RUN_DIR}/prettyHkFile${RUN}.root    
+    ./makeAcqdStartRunJsonFiles ${ROOT_RUN_DIR}/auxFile${RUN}.root    
+#    ./makeEventJsonFiles ${ROOT_RUN_DIR}/headFile${RUN}.root ${ROOT_RUN_DIR}/eventFile${RUN}.root  &
+fi
